89fe48795665681f63d12144898b5003981d0478
GopinadGvs <gopinadhgvs@gmail.com>
2024-12-30 11:25:05 +0530

    CREO Project Added

---
+++ CREO_API_CSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using pfcls;
+
+namespace testCase
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            //Get Active CREO Session
+            CCpfcAsyncConnection classAsyncConnection = new CCpfcAsyncConnection();
+            IpfcAsyncConnection connection = classAsyncConnection.Connect(null, null, null, null);
+            IpfcBaseSession session = (IpfcBaseSession)connection.Session;
+
+            //Display Message
+            (session as IpfcSession).UIShowMessageDialog("Welcome to CREO", null);
+        }
+    }
+}
