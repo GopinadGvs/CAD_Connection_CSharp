d14039a3e0cb1915088edef9de4971872cc3e6f8
GopinadGvs <gopinadhgvs@gmail.com>
2024-12-30 14:22:49 +0530

    AutoCAD Project Added

---
+++ AutoCAD_API_CSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.ComponentModel;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+
+
+namespace AutoCADAPI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [CommandMethod("Connection")]
+        static void Connection()
+        {
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+            Editor ed = acDoc.Editor;
+
+            ed.WriteMessage("Welcome to AutoCAD");
+        }
+    }
+}
