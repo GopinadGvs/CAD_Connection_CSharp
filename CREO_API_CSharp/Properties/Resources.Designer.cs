eb55ede860bdd80eb323d10c27bdd4a273147905
GopinadGvs <gopinadhgvs@gmail.com>
2024-12-30 11:26:05 +0530

    EXE Name updated

--- CREO_API_CSharp/Properties/Resources.Designer.cs
+++ CREO_API_CSharp/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace testCase.Properties {
+namespace CREOAPI.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace testCase.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("testCase.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CREOAPI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- CREO_API_CSharp/Properties/Settings.Designer.cs
+++ CREO_API_CSharp/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace testCase.Properties {
+namespace CREOAPI.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
