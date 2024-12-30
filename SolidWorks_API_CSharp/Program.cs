c5b455258484a1b5ca538b7a6a4886027bc254a3
GopinadGvs <gopinadhgvs@gmail.com>
2024-12-30 12:02:20 +0530

    Solidworks Project Added

---
+++ SolidWorks_API_CSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using SolidWorks.Interop.sldworks;
+
+namespace SolidWorksAPI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+
+        public static void Main()
+        {
+            //Get Active Solidworks Session (2022)
+            SldWorks sldWorkdApp = Marshal.GetActiveObject("SldWorks.Application.30") as SldWorks;
+
+            sldWorkdApp.SendMsgToUser("Welcome to Solidworks");
+        }
+    }
+}
