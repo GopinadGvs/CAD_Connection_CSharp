e6b94a8a863d2a995d4502bbd5d3f978c1155210
GopinadGvs <gopinadhgvs@gmail.com>
2024-12-30 11:14:25 +0530

    Inventor Project Added

---
+++ Inventor_API_CSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Inventor;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace InvnetorAPI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            //Get Active Inventor Session
+            Inventor.Application application = Marshal.GetActiveObject("Inventor.Application") as Inventor.Application;
+
+            MessageBox.Show("Welcome to Inventor", "MyTitle");
+        }
+    }
+}
